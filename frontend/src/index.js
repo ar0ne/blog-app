2979488dafc7fcd5c8116dc388be458f12cfa5c5
ar0ne <CAMTbITAKOU@yandex.ru>
2019-09-05 15:46:39 +0300

    add markdown

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import vuetify from './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue"
+import vuetify from "./plugins/vuetify"
+import App from "./App.vue"
+import router from "./router"
 
 new Vue({
     router,
     vuetify,
     render: h => h(App),
-}).$mount('#app')
+}).$mount("#app")
--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,37 +1,39 @@
 <template>
-  <v-container v-if="loading">
-    <div class="text-xs-center">
-      <v-progress-circular indeterminate :size="150" :width="8" color="green"></v-progress-circular>
-    </div>
-  </v-container>
+    <v-container v-if="loading">
+        <div class="text-xs-center">
+            <v-progress-circular indeterminate :size="150" :width="8" color="green"></v-progress-circular>
+        </div>
+    </v-container>
 
-  <v-container v-else>
-    <pre>{{ text }}</pre>
-  </v-container>
+    <v-container v-else>
+        <vue-markdown>{{text}}</vue-markdown>
+    </v-container>
 </template>
 
 <script>
-import AboutService from "../services/AboutService";
+import AboutService from "../services/AboutService"
+import VueMarkdown from "vue-markdown"
 
 export default {
-  data: function() {
-    return {
-      loading: true,
-      text: ""
-    };
-  },
-  mounted() {
-    AboutService.getReadme()
-      .then(response => {
-        this.text = response;
-        this.loading = false;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },
-  components: {
-    AboutService
-  }
-};
+    data: function() {
+        return {
+            loading: true,
+            text: "",
+        }
+    },
+    mounted() {
+        AboutService.getReadme()
+            .then(response => {
+                this.text = response
+                this.loading = false
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    },
+    components: {
+        AboutService,
+        VueMarkdown,
+    },
+}
 </script>
